7949ad510e7e4a13032e80bd4e29a72afe97e793
Asrar Ali <mohdasrar3825@gmail.com>
2025-06-16 17:19:20 +0530

    cors and cookie-parser

--- src/index.js
+++ src/index.js
@@ -18,4 +18,10 @@ dotenv.config({
     path:"./.env"
 })
 
-connectDB()
+connectDB().then(()=>{
+   app.listen(process.env.PORT || 8000 ,()=>{
+     console.log("server started on port", process.env.PORT || 8000);
+   })
+}).catch((error)=>{
+   console.log("MongoDB connection failed",error);
+})
