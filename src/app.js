c6fbefc90c128d8478d4cf827c89b6811bbfabd8
Asrar Ali <mohdasrar3825@gmail.com>
2025-06-19 14:58:45 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended : true ,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export default app
+
+// routes import
+import {router}  from "./routes/user.route.js"
+
+//routes declaration 
+
+app.use("/api/v1/users" , router)
+
+
+export default app  
--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,17 +1,18 @@
-const asyncHandler = (requestHandler) =>
-     (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next))
-    .catch((error)=>next(error));
+const asyncHandler = (requestHandler) => {
+   return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error));
+    };
 };
 
 
-const asyncHandlers = (requestHandler)=>async (req , res ,next)=>{
-    try {
-       await requestHandler(req , res ,next )
-    } catch (error) {
-        res.status(error.code || 500)
-    }
+// const asyncHandlers = (requestHandler)=>async (req , res ,next)=>{
+//     try {
+//        await requestHandler(req , res ,next )
+//     } catch (error) {
+//         res.status(error.code || 500)
+//     }
 
-}
+// }
 
 export {asyncHandler}
