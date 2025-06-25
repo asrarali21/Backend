90a189ad1330809285e4c22dbcee4c583778328c
Asrar Ali <mohdasrar3825@gmail.com>
2025-06-25 16:08:29 +0530

    access token and refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,14 @@ import {ApiResponse}  from "../utils/apiResponse.js"
 
 const GenerateAccessAndRefreshToken = async (userId)=>{
    try {
-    
+      const user = await User.findById(userId)
+     const accessToken = user.GeneratedAccessToken()
+     const refreshToken = user.GeneratedRefreshToken()
+
+     user.refreshToken = refreshToken
+     await user.save({validateBeforeSave : false})
+
+     return {accessToken , refreshToken}
    } catch (error) {
     throw new ApiError(500 , "something went wrong while generating refresh and access token ")
    }
