384563ad7dbbac9f866b0a760e180ee563dcdcd7
asrarali21 <mohdasrar3825@gmail.com>
2025-07-14 14:08:54 +0530

    fix cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,11 +2,10 @@ import {v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
 cloudinary.config({ 
-        cloud_name: process.env.COULDINARY_CLOUD_NAME,
-        api_key: process.env.COULDINARY_API_KEY,
-        api_secret: process.env.COULDINARY_API_SECRET
-    });
-
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const uploadoncloudinary = async (localFilePath)=>{
    try {
